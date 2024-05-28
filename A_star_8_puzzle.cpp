a43ea0c56ca276d3cb9f3fe3a84f629037017ae5
samyak619 <samyakhadse@gmail.com>
2024-05-28 21:58:11 +0530

    refactor: pass board by reference in findZero and printBoard functions

--- A_star_8_puzzle.cpp
+++ A_star_8_puzzle.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
-#include <iostream>
 using namespace std;
-
-void findZero(vector<vector<int>> &board, int &x, int &y)
+void findZero(vector<vector<int>> board, int &x, int &y)
 {
     for (int i = 0; i < board.size(); i++)
     {
@@ -18,7 +16,7 @@ void findZero(vector<vector<int>> &board, int &x, int &y)
     }
 }
 
-void printBoard(vector<vector<int>> &board)
+void printBoard(vector<vector<int>> board)
 {
     for (int i = 0; i < board.size(); i++)
     {
@@ -52,53 +50,43 @@ int findMisplacedTiles(vector<vector<int>> &board, vector<vector<int>> &goal)
 
 void aStar(vector<vector<int>> &board, vector<vector<int>> &goal, int depth, int x, int y)
 {
-    auto cmp = [](pair<pair<int, int>, vector<vector<int>>> &a, pair<pair<int, int>, vector<vector<int>>> &b)
-    {
-        return a.first.first > b.first.first;
-    };
-    priority_queue<pair<pair<int, int>, vector<vector<int>>>, vector<pair<pair<int, int>, vector<vector<int>>>>, decltype(cmp)> pq(cmp);
-
+    priority_queue<pair<pair<int, int>, vector<vector<int>>>, vector<pair<pair<int, int>, vector<vector<int>>>>,
+                   greater<pair<pair<int, int>, vector<vector<int>>>>>
+        pq;
     int g = depth;
     int h = findMisplacedTiles(board, goal);
     pq.push({{(g + h), g}, board});
-
     while (!pq.empty())
     {
         vector<vector<int>> curr = pq.top().second;
         int curr_d = pq.top().first.second;
         pq.pop();
-
         printBoard(curr);
-
         int x, y;
         findZero(curr, x, y);
-
         if (isGoalState(curr, goal))
         {
             cout << "Goal State Reached" << endl;
             return;
         }
-
         int dx[] = {0, 0, -1, 1};
         int dy[] = {1, -1, 0, 0};
-
         for (int i = 0; i < 4; i++)
         {
             int newX = x + dx[i];
             int newY = y + dy[i];
-
             if (newX >= 0 && newX < curr.size() && newY >= 0 && newY < curr.size())
             {
                 swap(curr[x][y], curr[newX][newY]);
                 int curr_h = findMisplacedTiles(curr, goal);
-                pq.push({{(curr_d + curr_h), curr_d + 1}, curr});
+                pq.push({{(curr_d + curr_h), curr_d}, curr});
                 swap(curr[x][y], curr[newX][newY]);
             }
         }
+        // ans.pop_back();
     }
     return;
 }
-
 int main()
 {
     vector<vector<int>> initial = {
@@ -109,7 +97,6 @@ int main()
         {1, 2, 3},
         {8, 0, 4},
         {7, 6, 5}};
-
     int x, y;
     findZero(initial, x, y);
 
